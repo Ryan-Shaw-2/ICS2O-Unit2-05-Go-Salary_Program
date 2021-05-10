dba464f2fb3bbcdcb9be211af71ea50f22636660
RyanShaw21 <>
2021-05-10 15:08:50 +0000

    fixed format

--- salaryProgram.go
+++ salaryProgram.go
@@ -6,31 +6,31 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/leekchan/accounting"
+    "github.com/leekchan/accounting"
 )
 
 func main() {
-	// This function displays currency
-	accountingFormater := accounting.Accounting{Symbol: "$", Precision: 2}
-	var hoursWorked int
-	var hourlyRate int
+    // This function displays currency
+    accountingFormater: = accounting.Accounting {Symbol: "$", Precision: 2}
+    var hoursWorked int
+    var hourlyRate int
 
-	// input
-	fmt.Println("This program calculates the users salary")
-	fmt.Println()
-	fmt.Print("Enter your number of hours worked: ")
-	fmt.Scanln(&hoursWorked)
-	fmt.Print("Enter your hourlyRate: ")
-	fmt.Scanln(&hourlyRate)
-	fmt.Println()
+    // input
+    fmt.Println("This program calculates the users salary")
+    fmt.Println()
+    fmt.Print("Enter your number of hours worked: ")
+    fmt.Scanln( & hoursWorked)
+    fmt.Print("Enter your hourlyRate: ")
+    fmt.Scanln( & hourlyRate)
+    fmt.Println()
 
-	// process
-  var pay = float64(hoursWorked * hourlyRate) * (1.00 - 0.18)
-  var tax = 0.18 * float64(hoursWorked * hourlyRate)
+    // process
+    var pay = float64(hoursWorked * hourlyRate) * (1.00 - 0.18)
+    var tax = 0.18 * float64(hoursWorked * hourlyRate)
 
-	// output
-	fmt.Println("Your pay will be:", accountingFormater.FormatMoney(pay))
-	fmt.Println("You will pay", accountingFormater.FormatMoney(tax), "in taxes")
+    // output
+    fmt.Println("Your pay will be:", accountingFormater.FormatMoney(pay))
+    fmt.Println("You will pay", accountingFormater.FormatMoney(tax), "in taxes")
 }
