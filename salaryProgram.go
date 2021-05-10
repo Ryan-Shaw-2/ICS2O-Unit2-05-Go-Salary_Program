b2c6f1f9c11f271d4e67b55f9ebcca5dcfc52c78
RyanShaw21 <>
2021-05-10 02:32:25 +0000

    fixed errors

--- salaryProgram.go
+++ salaryProgram.go
@@ -12,26 +12,25 @@ import (
 )
 
 func main() {
-  // This function displays currency
-  accountingFormater := accounting.Accounting{Symbol: "$", Precision: 2}
+	// This function displays currency
+	accountingFormater := accounting.Accounting{Symbol: "$", Precision: 2}
 	var hoursWorked int
 	var hourlyRate int
 
-	// input
-	fmt.Println("This program calculates the users salary")
-	fmt.Println()
+  // input
+  fmt.Println("This program calculates the users salary")
+  fmt.Println()
 	fmt.Print("Enter your number of hours worked: ")
 	fmt.Scanln(&hoursWorked)
 	fmt.Print("Enter your hourly rate: ")
 	fmt.Scanln(&hourlyRate)
 	fmt.Println()
 
-	// process
+  // process
 	var pay = float64(hoursWorked * hourlyRate) * (1.00 - 0.18)
 	var tax = 0.18 * float64(hoursWorked * hourlyRate)
 
-	// output
+  // output
 	fmt.Println("Your pay will be:", accountingFormater.FormatMoney(pay))
 	fmt.Println("You will pay", accountingFormater.FormatMoney(tax), "in taxes")
-
 }
